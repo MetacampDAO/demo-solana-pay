8691273671fa59f0a3510a3be957a9a2208d7f7b
Justin Lee <justinleeyile@gmail.com>
2022-12-03 22:13:17 +0800

    update sol pay

--- pages/api/interactive.ts
+++ pages/api/interactive.ts
@@ -9,67 +9,78 @@ import {
   Keypair,
 } from "@solana/web3.js";
 import bs58 from "bs58";
+import { createTransfer } from "@solana/pay";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  switch (req.method) {
-    case "GET":
-      console.log("GET");
-      const label = "Metacamp";
-      const icon =
-        "https://uploads-ssl.webflow.com/628b99344f25667e77da83cf/62c3a8d3be46ba64d97c2f28_metcamp-256-logo.png";
-      res.status(200).send({
-        label,
-        icon,
-      });
-      break;
+  try {
+    switch (req.method) {
+      case "GET":
+        console.log("GET");
+        const label = "Metacamp";
+        const icon =
+          "https://uploads-ssl.webflow.com/628b99344f25667e77da83cf/62c3a8d3be46ba64d97c2f28_metcamp-256-logo.png";
+        res.status(200).send({
+          label,
+          icon,
+        });
+        break;
 
-    case "POST":
-      const accountField = req.body.account;
-      const account = new PublicKey(accountField);
-      const connection = new Connection(clusterApiUrl("devnet"));
-      // create the transaction
-      const transaction = new Transaction();
+      case "POST":
+        const accountField = req.body.account;
+        const account = new PublicKey(accountField);
+        const connection = new Connection(clusterApiUrl("devnet"));
+        // create the transaction
+        const transaction = new Transaction();
 
-      const passDecodeKp = bs58.decode(process.env.SECRET_KEY!);
-      const passU8IntKp = new Uint8Array(
-        passDecodeKp.buffer,
-        passDecodeKp.byteOffset,
-        passDecodeKp.byteLength / Uint8Array.BYTES_PER_ELEMENT
-      );
-      const passKp = Keypair.fromSecretKey(passU8IntKp);
+        const passDecodeKp = bs58.decode(process.env.SECRET_KEY!);
+        const passU8IntKp = new Uint8Array(
+          passDecodeKp.buffer,
+          passDecodeKp.byteOffset,
+          passDecodeKp.byteLength / Uint8Array.BYTES_PER_ELEMENT
+        );
+        const passKp = Keypair.fromSecretKey(passU8IntKp);
 
-      const ix = SystemProgram.transfer({
-        fromPubkey: new PublicKey(
-          "HZGTNyvgFzpaYxqGiTpcYfbxYArPAAB4ffLSj5B9dv3"
-        ),
-        toPubkey: account,
-        lamports: 5e8,
-      });
+        const ix1 = SystemProgram.transfer({
+          fromPubkey: new PublicKey(
+            "HZGTNyvgFzpaYxqGiTpcYfbxYArPAAB4ffLSj5B9dv3"
+          ),
+          toPubkey: account,
+          lamports: 5e8,
+        });
+        const ix2 = SystemProgram.transfer({
+          fromPubkey: account,
+          toPubkey: new PublicKey(
+            "HZGTNyvgFzpaYxqGiTpcYfbxYArPAAB4ffLSj5B9dv3"
+          ),
+          lamports: 1e7,
+        });
 
-      transaction.add(ix);
+        transaction.add(ix1, ix2);
+        transaction.recentBlockhash = (
+          await connection.getLatestBlockhash("max")
+        ).blockhash;
+        transaction.feePayer = passKp.publicKey;
+        transaction.partialSign(passKp);
 
-      transaction.recentBlockhash = (
-        await connection.getLatestBlockhash()
-      ).blockhash;
-      transaction.feePayer = passKp.publicKey;
-      transaction.sign(passKp);
+        // Serialization
+        const serializedTransaction = transaction.serialize({
+          requireAllSignatures: false,
+        });
 
-      // Serialization
-      const serializedTransaction = transaction.serialize({
-        requireAllSignatures: false,
-      });
+        const base64Transaction = serializedTransaction.toString("base64");
+        const message = "Welcome to Metacamp";
 
-      const base64Transaction = serializedTransaction.toString("base64");
-      const message = "Welcome to Metacamp";
+        res.status(200).send({ transaction: base64Transaction, message });
 
-      res.status(200).send({ transaction: base64Transaction, message });
+        break;
 
-      break;
-
-    default:
-      break;
+      default:
+        break;
+    }
+  } catch (error) {
+    console.log("error", error);
   }
 }
